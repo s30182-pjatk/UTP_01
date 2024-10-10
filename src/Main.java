8091a4fc92c5c72f17013d417181350d78c58de4
s30182-pjatk <s30182@pjwstk.edu.pl>
2024-10-10 16:12:31 +0200

    Update Main.java, added TODO

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,9 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// TODO: we need to add the missing classes!
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        Adder adder = new Adder();
-        System.out.println(adder.add(1, 2));
-        Subtractor subtractor = new Subtractor();
-        System.out.println(subtractor.subtract(6, 3));
-    }
+ public static void main(String[] args) {
+ Adder adder = new Adder();
+ System.out.println(adder.add(1, 2));
+ Subtractor subtractor = new Subtractor();
+ System.out.println(subtractor.subtract(6, 3));
+ }
 }
